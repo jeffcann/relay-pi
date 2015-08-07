4effffae482145d1dbfaa4d8fdf0eafca6d06144
Jeffrey Cann <jcann@dius.com.au>
2015-08-07 15:51:03 +1000

    added native js gpio

---
+++ gpio.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+
+exports.init = function() {
+    [0, 1, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 21, 22, 23, 24, 25].forEach(initPin);
+};
+
+exports.initPin = function(pin) {
+    if(!exports.isPinOpen(pin)) {
+        exports.openPin(pin);
+        exports.setPinDirectionOut(pin);
+        exports.turnOff(pin);
+    }
+};
+
+exports.turnOn = function(pin) {
+    exports.writePin(pin, 1);
+};
+
+exports.turnOff = function(pin) {
+    exports.writePin(pin, 0);
+};
+
+exports.writePin = function(pin, value) {
+    f.writeFileSync('/sys/class/gpio/gpio' + pin + '/value', value);
+};
+
+exports.readPin = function(pin) {
+    return fs.readFileSync('/sys/class/gpio/gpio' + pin + '/value');
+};
+
+exports.isPinOpen = function(pin) {
+    return fs.existsSync('/sys/class/gpio/gpio' + pin);
+};
+
+exports.setPinDirectionOut = function(pin) {
+    f.writeFileSync('/sys/class/gpio/gpio' + pin + '/direction', 'out');
+};
+
+exports.setPinDirectionIn = function(pin) {
+    f.writeFileSync('/sys/class/gpio/gpio' + pin + '/direction', 'in');
+};
+
+exports.openPin = function(pin) {
+    f.writeFileSync('/sys/class/gpio/export', pin);
+};
+
+exports.closePin = function(pin) {
+    f.writeFileSync('/sys/class/gpio/unexport', pin);
+};
--- server.js
+++ server.js
@@ -2,33 +2,36 @@ var sh = require('execSync');
 var express = require('express');
 var app = express();
 var config = require('./config.json');
+var gpio = require('./gpio');
+
+gpio.init();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res) {
-  res.render('dashboard', { config:config });
+app.get('/', function (req, res) {
+    res.render('dashboard', {config: config});
 });
 
-app.get('/config', function(req, res) {
-  res.send(config);
+app.get('/config', function (req, res) {
+    res.send(config);
 });
 
-app.get('/switch/:pin/:state', function(req, res) {
-  var pin = req.params.pin;
-  var state = (req.params.state == 'on');
+app.get('/switch/:pin/:state', function (req, res) {
+    var pin = req.params.pin;
+    var state = (req.params.state == 'on');
 
-  if(state === true) {
-    sh.exec('./script/turnon.sh ' + pin);
-    res.send({status:'ok', pin:pin, state:'on'});
-  } else {
-    sh.exec('./script/turnoff.sh ' + pin);
-    res.send({status:'ok', pin:pin, state:'off'});
-  }
+    if (state === true) {
+        gpio.turnOn(pin);
+        res.send({status: 'ok', pin: pin, state: 'on'});
+    } else {
+        gpio.turnOff(pin);
+        res.send({status: 'ok', pin: pin, state: 'off'});
+    }
 });
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Controller app listening at http://%s:%s', host, port);
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Controller app listening at http://%s:%s', host, port);
 });
